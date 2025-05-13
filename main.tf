9e9050805cca501f8783aeb602e00e266c5395de
devarinti02 <mokshith.bobby@gmail.com>
2025-05-13 18:10:47 +0530

    check02

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws"{
+    region = "ap-southeat-1"
+}
+
+resource "aws_instance" "practice01"{
+    ami = "ami-01938df366ac2d954"
+    instance_type = "t2.micro"
+    availability_zone = "ap-southeast-1"
+        tags{
+            name = "terraform-practice01"
+        }
+}
